43313ec6e4eeabbc38ded7adea759c69984c676b
Kevin Compton <kevin@wecreativeagency.com>
2023-07-02 16:20:34 -0700

    proposal create stepped flow

--- src/app/proposals/page.tsx
+++ src/app/proposals/page.tsx
@@ -33,16 +33,16 @@ export default function Proposals() {
           <h3 className="font-bold mb-1 text-lg">{grant.title}</h3>
           <div className="text-sm align-middle"><MapPinIcon className="h-4 inline-block" /> {grant.location}</div>
           <p className="text-sm mt-2 mb-1 leading-1">{grant.description}</p>
-          {grant.collaborators.map((user, index) => (
+          {//grant.collaborators.map((user, index) => (
             <>
-              {(index+1) === grant.collaborators.length && (
-                <span className="text-sm"><a className="text-sky-600" href="#">{user}</a></span>
-              )}
-              {(index+1) !== grant.collaborators.length && (
-                <span className="text-sm"><a className="text-sky-600" href="#">{user}</a>, </span>
-              )}
+              {//(index+1) === grant.collaborators.length && (
+                <span className="text-sm"><a className="text-sky-600" href="#">{/*user*/}</a></span>
+              /*)*/}
+              {//(index+1) !== grant.collaborators.length && (
+                <span className="text-sm"><a className="text-sky-600" href="#">{/*user*/}</a>, </span>
+              /*)*/}
             </>
-          ))}
+          /*))*/}
         </div>
       ))}
       <div className="fixed bottom-4 right-0 left-0 bg-white p-5 z-0">
--- src/app/proposals/write/page.tsx
+++ src/app/proposals/write/page.tsx
@@ -47,11 +47,48 @@ export default function WriteProposal() {
 		}
 	};
 	const [rows, setRows] = useState([{ key: "default" }]);
+	const [currentStep, setCurrentStep] = useState(1);
 	const inputClasses = "w-full border border-slate-300 rounded h-10 pl-2 mb-6";
+	const textareaClasses = "w-full border border-slate-300 rounded h-20 pl-2 mb-6";
 
 	function removeRow(index: string) {
 		if (index !== "default")
 			setRows((current) => current.filter((_) => _.key !== index));
+	}
+
+	function setStep(direction:string) {
+		console.log(currentStep)
+		if(direction === "next") {
+			setCurrentStep(currentStep+1)
+		}
+		if(direction === "previous") {
+			setCurrentStep(currentStep-1)
+		}
+		console.log(currentStep)
+	}
+
+	const StepControls = () => {
+		return(
+			<div className="flex mb-10 mt-10">
+				{currentStep !== 1 && (
+					<button
+						className="border border-slate-400 rounded leading-10 font-bold px-10"
+						onClick={() => setStep("previous")}
+					>
+						Previous
+					</button>
+				)}
+				
+				{currentStep !== 6 && (
+					<button
+						className="border border-slate-400 rounded leading-10 font-bold px-10 ml-auto"
+						onClick={() => setStep("next")}
+					>
+						Next
+					</button>
+				)}
+			</div>
+		)
 	}
 
 	const MilestoneRow = ({ index, ...props }: MilestoneProps) => {
@@ -102,69 +139,105 @@ export default function WriteProposal() {
 
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
-			<h3 className="font-bold mb-6">Grant Information</h3>
-			<input
-				className={inputClasses}
-				placeholder="Title"
-				{...register("title")}
-			/>
-            <input
-				className={inputClasses}
-				placeholder="Location"
-				{...register("location")}
-			/>
-			<input className={inputClasses} placeholder="Add Collaborator" />
-			<textarea
-				className={inputClasses}
-				placeholder="Description"
-				{...register("description")}
-			/>
-			<input
-				className={inputClasses}
-				placeholder="Locations Affected"
-				{...register("affected_locations")}
-			/>
-			<input
-				className={inputClasses}
-				placeholder="Community Problem to Address"
-				{...register("community_problem")}
-			/>
-			<input
-				className={inputClasses}
-				placeholder="Proposed Solution"
-				{...register("proposed_solution")}
-			/>
-			<input
-				className={inputClasses}
-				placeholder="Minimum Total Budget"
-				{...register("minimum_budget")}
-			/>
-			<input
-				className={inputClasses}
-				placeholder="Key Players (Including Vendors)"
-				{...register("key_players")}
-			/>
-			<input
-				className={inputClasses}
-				placeholder="Timeline"
-				{...register("timeline")}
-			/>
-			<h3 className="font-bold mb-4">Milestones</h3>
-			{rows.map((row, index) => (
-				<MilestoneRow key={row.key} index={row.key} />
-			))}
-			<p
-				className="text-right underline mb-8 text-sky-600 mt-2"
-				onClick={addRow}
-			>
-				Add Milestone
-			</p>
-			<button
-				className="w-full border border-slate-400 rounded leading-10 font-bold"
-				type="submit"
-			>
-				Submit
-			</button>
+			{currentStep === 1 && (
+				<>
+					<h3 className="font-bold mb-6">Basic Information</h3>
+					<input
+						className={inputClasses}
+						placeholder="Title"
+						{...register("title")}
+					/>
+					<input
+						className={inputClasses}
+						placeholder="Location"
+						{...register("location")}
+					/>
+					<input className={inputClasses} placeholder="Add Collaborator" />
+					<StepControls />
+				</>
+			)}
+			{currentStep === 2 && (
+				<>
+					<h3 className="font-bold mb-6">Grant Information</h3>
+					<textarea
+						className={textareaClasses}
+						placeholder="Description"
+						{...register("description")}
+					/>
+					<input
+						className={inputClasses}
+						placeholder="Locations Affected"
+						{...register("affected_locations")}
+					/>
+					<StepControls />
+				</>
+			)}
+			{currentStep === 3 && (
+				<>
+					<h3 className="font-bold mb-6">Community Problem</h3>
+					<textarea
+						className={textareaClasses}
+						placeholder="Community Problem to Address"
+						{...register("community_problem")}
+					/>
+					<p className="text-sm center italic">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+					<StepControls />
+				</>
+			)}
+			{currentStep === 4 && (
+				<>
+					<h3 className="font-bold mb-6">Proposed Solution</h3>
+					<textarea
+						className={textareaClasses}
+						placeholder="Proposed Solution"
+						{...register("proposed_solution")}
+					/>
+					<p className="text-sm center italic">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+					<StepControls />
+				</>
+			)}
+			{currentStep === 5 && (
+				<>
+					<h3 className="font-bold mb-6">Project Details</h3>
+					<input
+						className={inputClasses}
+						placeholder="Minimum Total Budget"
+						{...register("minimum_budget")}
+					/>
+					<input
+						className={inputClasses}
+						placeholder="Key Players (Including Vendors)"
+						{...register("key_players")}
+					/>
+					<input
+						className={inputClasses}
+						placeholder="Timeline"
+						{...register("timeline")}
+					/>
+					<StepControls />
+				</>
+			)}
+			{currentStep === 6 && (
+				<>
+					<h3 className="font-bold mb-6">Milestones</h3>
+					{rows.map((row, index) => (
+						<MilestoneRow key={row.key} index={row.key} />
+					))}
+					<p
+						className="text-right underline mb-8 text-sky-600 mt-2"
+						onClick={addRow}
+					>
+						Add Milestone
+					</p>
+					<p className="underline mb-8 text-sky-600" onClick={() => setStep("previous")}>Previous</p>
+					<button
+						className="w-full border border-slate-400 rounded leading-10 font-bold"
+						type="submit"
+					>
+						Submit
+					</button>
+				</>
+			)}
 		</form>
 	);
 }
