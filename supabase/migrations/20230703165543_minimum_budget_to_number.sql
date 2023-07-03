b5ac6dbef2f133b9b939fd626f138f16aef5e356
Matthew Watman <mattwatman@gmail.com>
2023-07-03 16:01:59 -0400

    Update migration logic

--- supabase/migrations/20230703165543_minimum_budget_to_number.sql
+++ supabase/migrations/20230703165543_minimum_budget_to_number.sql
@@ -1,13 +1,1 @@
-alter table proposals
-add column minimum_budget_new integer;
-
-update proposals
-set minimum_budget_new = cast(minimum_budget as integer);
-
-alter table proposals 
-drop column minimum_budget;
-
-alter table proposals
-rename column minimum_budget_new to minimum_budget;
-
-
+alter table proposals alter column minimum_budget type integer using minimum_budget::integer;
