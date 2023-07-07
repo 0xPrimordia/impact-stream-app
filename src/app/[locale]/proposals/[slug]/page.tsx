bb366f5f59f44a809cd89539c55b59dbc54c5793
Matthew Watman <mattwatman@gmail.com>
2023-07-06 20:26:08 -0400

    Add Supabase data to write and detail pages

--- src/app/[locale]/proposals/[slug]/page.tsx
+++ src/app/[locale]/proposals/[slug]/page.tsx
@@ -3,67 +3,76 @@ import React, { useState, useEffect } from "react";
 import { supabase } from "../../../../../lib/supabase-client";
 import { useRouter } from "next/navigation";
 import { usePrivy } from "@privy-io/react-auth";
-import { MapPinIcon, UserCircleIcon, UserGroupIcon } from "@heroicons/react/24/outline"
-import { Proposal } from "@/app/types";
+import {
+  MapPinIcon,
+  UserCircleIcon,
+  UserGroupIcon,
+} from "@heroicons/react/24/outline";
+import { FullProposal } from "@/app/types";
 
 export default function Page({ params }: { params: { slug: string } }) {
-  const { user, ready, authenticated } = usePrivy();
+  const { ready, authenticated } = usePrivy();
   const router = useRouter();
-  const [proposal, setProposal] = useState<Proposal>();
+  const [proposal, setProposal] = useState<FullProposal>();
 
   useEffect(() => {
-		getProposal();
-	}, []);
+    getProposal();
+  }, []);
 
   async function getProposal() {
-		const { data, error } = await supabase.from("proposals").select().eq("id", params.slug);
-		if (data) setProposal(data[0]);
-    if(error)
-		console.log(error);
-	}
-
-  const grant = {
-    id: 1,
-    author: "Jane Doe",
-    title: "Well Project",
-    location: "Nammpoak, Togo",
-    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    collaborators: ["John Doe", "Ted Doe"],
-    milestones: [
-      {
-        id: 1,
-        title: "Milestone 1 title",
-        budget: 2000
-      },
-      {
-        id: 2,
-        title: "Milestone 2 title",
-        budget: 3000
-      },
-      {
-        id: 3,
-        title: "Milestone 3 title",
-        budget: 1000
-      }
-    ],
-    budget: 6000
+    const { data, error } = await supabase
+      .from("proposals")
+      .select(
+        `title,
+        location,
+        description, 
+        affected_locations, 
+        author_id(name, family_name), 
+        community_problem, 
+        proposed_solution, 
+        project_milestones, 
+        key_players, 
+        minimum_budget, 
+        timeline, 
+        users(name, family_name)`
+      )
+      .eq("id", params.slug)
+      .single();
+    console.log(data);
+    if (data) setProposal(data);
+    if (error) console.log(error);
   }
 
   if (!ready) return null;
-	if (ready && !authenticated) {
-		router.push("/");
-	}
+  if (ready && !authenticated) {
+    router.push("/");
+  }
 
-  
-  return(
+  return (
     <div>
-      <div className="font-bold mb-6"><a className="text-sky-600" href="/proposals">Proposed Grants</a> / {proposal?.title}</div>
-      <div className="text-sm align-middle mb-4"><MapPinIcon className="h-4 inline-block" /> {proposal?.location}</div>
-      <div className="text-sm mb-4"><UserCircleIcon className="h-4 inline-block" /> {grant.author}</div>
+      <div className="font-bold mb-6">
+        <a className="text-sky-600" href="/proposals">
+          Proposed Grants
+        </a>{" "}
+        / {proposal?.title}
+      </div>
+      <div className="text-sm align-middle mb-4">
+        <MapPinIcon className="h-4 inline-block" /> {proposal?.location}
+      </div>
+      <div className="text-sm mb-4">
+        <UserCircleIcon className="h-4 inline-block" />
+        {proposal?.author_id?.name + " " + proposal?.author_id?.family_name}
+      </div>
       <div>
-        <span className="text-sm"><UserGroupIcon className="h-4 inline-block" /> list collaborators </span>
-        
+        <span className="text-sm">
+          <UserGroupIcon className="h-4 inline-block" />{" "}
+          {Array.isArray(proposal?.users)
+            ? proposal.users
+                .map((user) => user.name + " " + user.family_name)
+                .join(", ")
+            : proposal?.users.name + " " + proposal?.users.family_name}
+        </span>
+
         <p className="text-sm leading-1 mt-4">{proposal?.description}</p>
 
         <h3 className="font-bold mt-6 text-sm">Affected Locations</h3>
@@ -79,13 +88,18 @@ export default function Page({ params }: { params: { slug: string } }) {
         <p className="text-sm leading-1 mt-2">{proposal?.key_players}</p>
       </div>
       <h3 className="font-bold mt-6 mb-5">Milestones</h3>
-      {grant.milestones.map((milestone) => (
-        <div key={milestone.id} className="mt-3 mb-3">
-          {milestone.title}: ${milestone.budget}
-        </div>
-      ))}
-      <div className="italic mt-6">Total Budget: {grant.budget}</div>
+      {proposal?.project_milestones &&
+        Object.values(proposal.project_milestones).map((milestone) => (
+          <div key={milestone.title} className="mt-3 mb-3">
+            {milestone.title}: ${milestone.budget}
+          </div>
+        ))}
+      <div className="italic mt-6">
+        Minimum Budget: {proposal?.minimum_budget}
+      </div>
+      <div className="italic mt-6">
+        Total Budget: {proposal?.minimum_budget}
+      </div>
     </div>
-    
-  )
+  );
 }
--- src/app/types.ts
+++ src/app/types.ts
@@ -1,60 +1,61 @@
 export type Proposal = {
-    id: string;
-	title: string|null;
-    location: string|null;
-	description: string|null;
-	timeline: string|null;
-	affected_locations: string|null;
-	community_problem: string|null;
-	proposed_solution: string|null;
-	minimum_budget: number|null;
-	key_players: string|null;
+	id: string;
+	title: string | null;
+	location: string | null;
+	description: string | null;
+	timeline: string | null;
+	affected_locations: string | null;
+	community_problem: string | null;
+	proposed_solution: string | null;
+	minimum_budget: number | null;
+	key_players: string | null;
 };
 
 export type SummaryProposal = {
 	id: string;
-	title: string|null;
-    location: string|null;
-	description: string|null;
-	users: any|null;
-}
+	title: string | null;
+	location: string | null;
+	description: string | null;
+	users: any | null;
+};
 
 export type FullProposal = {
-	id: string;
-	title: string|null;
-    location: string|null;
-	description: string|null;
-	timeline: string|null;
-	affected_locations: string|null;
-	community_problem: string|null;
-	proposed_solution: string|null;
-	minimum_budget: number|null;
-	key_players: string|null;
-	milestones: Milestone[]|null;
-	users: Collaborator[]|null;
-}
+	title: string | null;
+	author: {
+		name: string | null;
+		family_name: string | null;
+	};
+	location: string | null;
+	description: string | null;
+	timeline: string | null;
+	affected_locations: string | null;
+	community_problem: string | null;
+	proposed_solution: string | null;
+	minimum_budget: number | null;
+	key_players: string | null;
+	project_milestones: Milestone[] | null;
+	users: Collaborator[] | null;
+};
 
 type Collaborator = {
-	name: string | null; 
+	name: string | null;
 	family_name: string | null;
-}
+};
 
 type Milestone = {
 	title: string;
 	budget: number;
-}
+};
 
 export type User = {
-	id:string;
-	name: string | null; 
+	id: string;
+	name: string | null;
 	family_name: string | null;
-	address: string|null;
-	created_at: string|null;
+	address: string | null;
+	created_at: string | null;
 	onboarded: boolean | null;
 	phone_number: string | null;
 	village_neighborhood: string | null;
-}
+};
 
-export type PrivyUser = {
-	
-}
+export type PrivyUser = {};
