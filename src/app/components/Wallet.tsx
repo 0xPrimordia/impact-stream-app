e73829659e487bffa77562e53bbe434fe4ad26b8
Matthew Watman <mattwatman@gmail.com>
2023-07-09 13:19:46 -0400

    Add custom persister for supabase

--- src/app/components/Wallet.tsx
+++ src/app/components/Wallet.tsx
@@ -1,29 +1,27 @@
 "use client";
 
+import { useEffect } from "react";
 import { usePrivy } from "@privy-io/react-auth";
 import { useRouter } from "next/navigation";
 import { supabase } from "../../../lib/supabase-client";
 import { useTranslations } from "next-intl";
+import { createQueries, createCustomPersister, Store } from "tinybase";
+import { createLocalPersister } from "tinybase/persisters/persister-browser";
 import {
-  createQueries,
-  createLocalPersister,
-  createCustomPersister,
-} from "tinybase";
-import {
-  useCreateQueries,
-  useStore,
-  useResultRow,
-  useCreatePersister,
+	useCreateQueries,
+	useStore,
+	useResultRow,
+	useCreatePersister,
 } from "tinybase/ui-react";
 
 async function supabaseAuth(address: string, userId: string) {
-  await fetch("/api/auth", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ address, userId }),
-  });
+	await fetch("/api/auth", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ address, userId }),
+	});
 }
 
 // async function checkOnboardingStatus(userId: string) {
@@ -43,73 +41,74 @@ async function supabaseAuth(address: string, userId: string) {
 // }
 
 export function Wallet() {
-  const router = useRouter();
-  const { login, authenticated, user, ready } = usePrivy();
-  const t = useTranslations("Sign-In");
-  const store = useStore();
-  const localPersister = useCreatePersister(store, (store) => {
-    return createLocalPersister(store, "users");
-  });
-  const supabasePersister = useCreatePersister(store, (store) => {
-    return createCustomPersister(
-	  store,
-	  async () => {
-		const { data, error } = await supabase
-		  .from("users")
-		  .select("onboarded")
-		  .eq("id", user ? user.id : "")
-		  .single();
-		if (error) {
-		  throw error;
-		}
-		return data;
-	 },
-	 async (getContent) => {
-	  const { users } = getContent();
-		const onboardingStatus = users[user.id]["onboarded"];
-		const { data, error } = await supabase
-		  .from("users")
-		  .update(onboardingStatus)
-		  .eq("id", user ? user.id : "");
-		if (error) {
-		  throw error;
-		}
-	 },
-		 (listener) => setInterval(listener, 1000),
-		 (interval) => clearInterval(interval),
+	const router = useRouter();
+	const { login, authenticated, user, ready } = usePrivy();
+	const t = useTranslations("Sign-In");
+	const store = useStore() as Store;
+	const localPersister = useCreatePersister(store, (store) => {
+		return createLocalPersister(store, "users");
+	});
+	const supabasePersister = useCreatePersister(store, (store) => {
+		return createCustomPersister(
+			store,
+			async () => {
+				const { data, error } = await supabase
+					.from("users")
+					.select("onboarded")
+					.eq("id", user ? user.id : "")
+					.single();
+				if (error) {
+					throw error;
+				}
+				return data.onboarded;
+			},
+			async (getContent) => {
+				const [users] = getContent();
+				const onboardingStatus = users[user.id]["onboarded"];
+				const { error } = await supabase
+					.from("users")
+					.update(onboardingStatus)
+					.eq("id", user ? user.id : "");
+				if (error) {
+					throw error;
+				}
+			},
+			(listener) => setInterval(listener, 1000),
+			(interval) => clearInterval(interval)
+		);
+	});
+	const queries = useCreateQueries(store!, (store) =>
+		createQueries(store).setQueryDefinition(
+			"onboardingStatus",
+			"users",
+			({ select, where }) => {
+				select("onboarded");
+				where("id", user ? user.id : "");
+			}
+		)
 	);
-  const queries = useCreateQueries(store!, (store) =>
-    createQueries(store).setQueryDefinition(
-      "onboardingStatus",
-      "users",
-      ({ select, where }) => {
-        select("onboarded");
-        where("id", user ? user.id : "");
-      }
-    )
-  );
-  const onboarded = useResultRow("onboardingStatus", "users", queries);
-  if (ready && authenticated && user && user.wallet?.address && !onboarded) {
-    supabaseAuth(user.wallet?.address as string, user.id);
-    store?.setPartialRow("users", user.id, {
-      address: user.wallet?.address,
-      onboarded: true,
-    });
-    await localPersister.save();
+	const onboarded = useResultRow("onboardingStatus", "users", queries);
+	if (ready && authenticated && user && user.wallet?.address && !onboarded) {
+		supabaseAuth(user.wallet?.address as string, user.id);
+		store?.setPartialRow("users", user.id, {
+			address: user.wallet?.address,
+			onboarded: true,
+		});
 		localPersister.destroy();
-  } else {
-    router.push(`/proposals/`);
-  }
+		router.push(`/onboarding/`);
+	} else {
+		router.push(`/proposals/`);
+	}
 
-  return (
-    <>
-      <p className="text-sm text-center italic my-10">{t("disclaimer")}</p>
-      <button
-        className="w-full border border-slate-400 rounded leading-10 font-bold"
-        onClick={login}
-      >
-        {t("signInButton")}
-      </button>
-    </>
-  );
+	return (
+		<>
+			<p className="text-sm text-center italic my-10">{t("disclaimer")}</p>
+			<button
+				className="w-full border border-slate-400 rounded leading-10 font-bold"
+				onClick={login}
+			>
+				{t("signInButton")}
+			</button>
+		</>
+	);
 }
