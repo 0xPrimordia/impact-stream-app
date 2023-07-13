78fe2388f7bbcec025759b6746e2b6379015ad87
Matthew Watman <mattwatman@gmail.com>
2023-07-13 14:26:19 -0400

    Update browser check logic

--- src/app/[locale]/components/BrowserCheck.tsx
+++ src/app/[locale]/components/BrowserCheck.tsx
@@ -6,24 +6,6 @@ export default function BrowserCheck({ children }: React.ReactNode) {
 var userAgent = navigator.userAgent;
 var browserName, browserVersion;
 
-if (userAgent.indexOf("Edge") > -1) {
-  browserName = "Microsoft Edge";
-  browserVersion = userAgent.substring(userAgent.indexOf("Edge") + 5).split(" ")[0];
-  if (browserVersion < 10) {
-    throw new Error("Unsupported browser version");
-  }
-} else if (userAgent.indexOf("MSIE") > -1 || userAgent.indexOf("Trident") > -1) {
-  browserName = "Internet Explorer";
-  browserVersion = userAgent.indexOf("MSIE") > -1 ? userAgent.substring(userAgent.indexOf("MSIE") + 5).split(";")[0] : userAgent.substring(userAgent.indexOf("rv:") + 3).split(")")[0];
-  if (browserVersion < 11) {
-    throw new Error("Unsupported browser version");
-  }
-} else if (userAgent.indexOf("Firefox") > -1) {
-  browserName = "Mozilla Firefox";
-  browserVersion = userAgent.split("Firefox/")[1];
-  if (browserVersion < 60) {
-    throw new Error("Unsupported browser version");
-  }
 } else if (userAgent.indexOf("Chrome") > -1) {
   browserName = "Google Chrome";
   browserVersion = userAgent.split("Chrome/")[1].split(" ")[0];
@@ -34,10 +16,10 @@ if (userAgent.indexOf("Edge") > -1) {
   browserName = "Safari";
   browserVersion = userAgent.split("Version/")[1].split(" ")[0];
   if (browserVersion < 12) {
-    throw new Error("Unsupported browser version");
+    throw new Error("Unsupported browser version. Please use Safari 12 or higher.");
   }
 } else {
-  throw new Error("Unsupported browser");
+  throw new Error("Unsupported browser. Please use modern versions of Chrome or Safari.");
 }
 
 console.log("Browser Name: " + browserName);
