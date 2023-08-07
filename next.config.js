1d79b657668f45e8488782f3cd332d16f953ca8e
Matthew Watman <mattwatman@gmail.com>
2023-08-07 11:38:37 -0400

    Remove old cache rules, update next config

--- next.config.js
+++ next.config.js
@@ -4,26 +4,9 @@ const nextConfig = {
     NEXT_WEB3_STORAGE_TOKEN: process.env.NEXT_WEB3_STORAGE_TOKEN,
   },
 };
-const cacheRules = require("./cache");
 
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  extendDefaultRuntimeCaching: true,
-  workboxOptions: {
-    runtimeCaching: [
-      {
-        urlPattern: "https://auth.privy.io/api/v1/sessions",
-        handler: "StaleWhileRevalidate",
-        options: {
-          cacheName: "privy-session",
-          expiration: {
-            maxEntries: 1,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
-        },
-      },
-    ],
-  },
 });
 
 module.exports = withPWA(nextConfig);
