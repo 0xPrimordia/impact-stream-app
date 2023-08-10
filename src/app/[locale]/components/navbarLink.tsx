22106a5ec39419ea23dad8371aeb869f9a156e00
RoyNyaga <nyagaandreroy@gmail.com>
2023-08-10 22:08:18 +0100

    create NavbarLink component

---
+++ src/app/[locale]/components/navbarLink.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+interface NavbarLinkProps {
+	children: React.ReactNode;
+	path: string;
+	setOverlay: Function;
+}
+const NavbarLink = ({ children, setOverlay, path, ...props}: NavbarLinkProps) => {
+
+  return <Link onClick={()=>setOverlay(false)} href={path}>{children}</Link>
+}
+
+export default NavbarLink;
