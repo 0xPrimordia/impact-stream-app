f622b2e9198c49fc93beea14e550529a72a04d02
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-11-02 13:48:43 +0530

    chore: fix cart count
    fixes https://github.com/0xPrimordia/impact-stream-app/issues/265

--- src/app/[locale]/components/Navbar.tsx
+++ src/app/[locale]/components/Navbar.tsx
@@ -6,19 +6,13 @@ import { useTranslations } from "next-intl";
 import NavbarLink from "./NavbarLink";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { useCart } from "@/app/context/CartContext";
 
 export const Navbar = () => {
   const [overlay, setOverlay] = useState(false);
   const t = useTranslations("Navigation");
   const router = useRouter();
-  const [cartCount, setCartCount] = useState<number>();
-
-  useEffect(() => {
-    console.log('cart items: ' + localStorage.getItem("cartItems"))
-    const cartItems = localStorage.getItem("cartItems")
-    if(cartItems && JSON.parse(cartItems).length > 0)
-    setCartCount(JSON.parse(cartItems).length)
-  }, []);
+  const context = useCart();
 
   return (
     <div className="fixed top-0 left-0 right-0 bg-white p-8 pb-4 z-50">
@@ -78,8 +72,8 @@ export const Navbar = () => {
           router.push(`/cart`);
         }}
       >
-        {cartCount && (
-          <div className="text-xs bg-blue-600 font-bold rounded-full absolute right-16 z-10 block h-5 leading-5 w-5 text-center text-white top-9">{cartCount}</div>
+        {context.cartItems.length && (
+          <div className="text-xs bg-blue-600 font-bold rounded-full absolute right-16 z-10 block h-5 leading-5 w-5 text-center text-white top-9">{context.cartItems.length}</div>
         )}
         
         <ArchiveBoxIcon className="h-6 absolute right-20 top-11" />
