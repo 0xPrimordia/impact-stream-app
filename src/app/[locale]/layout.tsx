f961665dfd352c2805cf8a1520bccb6ed5edc220
Kevin Compton <kevin@wecreativeagency.com>
2023-12-01 12:42:24 -0800

    updates

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -44,7 +44,7 @@ export default async function RootLayout({
                       <Navbar />
                     </div>
                     {children}
-            
+                    <Footer />
                   </ProposalsProvider>
                 </CartContextProvider>
               </div>
